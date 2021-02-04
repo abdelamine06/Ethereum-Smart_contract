95099bff896be15c44ac1fb6c74cafd628008dc2
Amine <abdelamine.mehdaoui@gmail.com>
2021-02-04 22:36:44 +0100

    Add comparison instructions AND, OR, EQUAL ..

--- interpeter/main.js
+++ interpeter/main.js
@@ -4,6 +4,12 @@ const PUSH = 'PUSH';
 const SUB = 'SUB';
 const MUL = 'MUL';
 const DIV = 'DIV';
+const LT = 'LT';
+const GT = 'GT';
+const EQ = 'EQ';
+const AND = 'AND';
+const OR = 'OR';
+
 
 class Interpreter {
     constructor() {
@@ -31,14 +37,24 @@ class Interpreter {
                     case SUB:
                     case MUL:
                     case DIV:
+                    case LT:
+                    case GT:
+                    case EQ:
+                    case AND:
+                    case OR:
                         const a = this.state.stack.pop();
                         const b = this.state.stack.pop();
 
                         let result;
-                        if (opCode === ADD) result = a + b
-                        if (opCode === SUB) result = a - b
-                        if (opCode === MUL) result = a * b
-                        if (opCode === DIV) result = a / b
+                        if (opCode === ADD) result = a + b;
+                        if (opCode === SUB) result = a - b;
+                        if (opCode === MUL) result = a * b;
+                        if (opCode === DIV) result = a / b;
+                        if (opCode === LT) result = a < b ? 1 : 0;
+                        if (opCode === GT) result = a > b ? 1 : 0;
+                        if (opCode === EQ) result = a === b ? 1 : 0;
+                        if (opCode === AND) result = a && b;
+                        if (opCode === OR) result = a || b;
 
                         this.state.stack.push(result);
                         break;
@@ -70,3 +86,23 @@ console.log('Result of 2 MUL 3 :', result);
 code = [PUSH, 2, PUSH, 3, DIV, STOP];
 result = new Interpreter().runCode(code)
 console.log('Result of 3 DIV 2 :', result);
+
+code = [PUSH, 2, PUSH, 3, LT, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result of 3 Less then 2 :', result);
+
+code = [PUSH, 2, PUSH, 3, GT, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result of 3 Greater then 2 :', result);
+
+code = [PUSH, 2, PUSH, 2, EQ, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result of 2 === 2 :', result);
+
+code = [PUSH, 1, PUSH, 0, AND, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result of 0 AND 1 :', result);
+
+code = [PUSH, 1, PUSH, 0, OR, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result of 0 OR 1 :', result);
