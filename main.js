bbb17161055ecac0434632dda996214e3e3dcebf
Amine <abdelamine.mehdaoui@gmail.com>
2021-02-04 01:05:50 +0100

    Lightning-smart-contract language

---
+++ main.js
@@ -0,0 +1,47 @@
+const STOP = 'STOP';
+const ADD = 'ADD';
+const PUSH = 'PUSH';
+
+class Interpreter {
+    constructor() {
+
+        this.state = {
+            programCounter: 0,
+            stack: [],
+            code: []
+        };
+    }
+    runCode(code) {
+        this.state.code = code;
+        while (this.state.programCounter < this.state.code.length) {
+            const opCode = this.state.code[this.state.programCounter];
+            try {
+                switch (opCode) {
+                    case STOP:
+                        throw new Error("Execution is complete");
+                    case PUSH:
+                        this.state.programCounter++;
+                        const value = this.state.code[this.state.programCounter];
+                        this.state.stack.push(value);
+                        break;
+                    case ADD:
+                        const a = this.state.stack.pop();
+                        const b = this.state.stack.pop();
+                        this.state.stack.push(a + b);
+                        break;
+                    default:
+                        break;
+
+                }
+
+            } catch (error) {
+                console.log(this.state.stack[this.state.stack.length - 1]);
+            }
+            this.state.programCounter++;
+
+        }
+    }
+}
+const code = [PUSH, 2, PUSH, 3, ADD, STOP];
+const interepter = new Interpreter();
+interepter.runCode(code)
